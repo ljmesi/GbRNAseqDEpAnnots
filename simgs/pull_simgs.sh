c2be656affef46c34c1d4b95885a99d7a2a3fb97
ljmesi <37740329+ljmesi@users.noreply.github.com>
2021-11-17 16:31:57 +0100

    Pull only gffread.sif

--- simgs/pull_simgs.sh
+++ simgs/pull_simgs.sh
@@ -3,7 +3,8 @@
 set -e
 set -uo pipefail
 
-./pull_simgs_docker.sh fastqc.sif quay.io/biocontainers/fastqc:0.11.9--hdfd78af_1
-./pull_simgs_docker.sh multiqc.sif quay.io/biocontainers/multiqc:1.11--pyhdfd78af_0
-./pull_simgs_docker.sh seqkit.sif quay.io/biocontainers/seqkit:2.0.0--h9ee0642_0
+#./pull_simgs_docker.sh fastqc.sif quay.io/biocontainers/fastqc:0.11.9--hdfd78af_1
+#./pull_simgs_docker.sh multiqc.sif quay.io/biocontainers/multiqc:1.11--pyhdfd78af_0
+#./pull_simgs_docker.sh seqkit.sif quay.io/biocontainers/seqkit:2.0.0--h9ee0642_0
+./pull_simgs_docker.sh gffread.sif quay.io/biocontainers/gffread:0.12.7--h9a82719_0
 
