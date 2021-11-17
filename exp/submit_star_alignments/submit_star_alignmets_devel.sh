161fc792f21f434d7cf2eee1b8dab6d40b3e8d5d
ljmesi <37740329+ljmesi@users.noreply.github.com>
2021-11-17 18:24:31 +0100

    Add sending test sbatch jobs script

---
+++ exp/submit_star_alignments/submit_star_alignmets_devel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+module load bioinfo-tools gnuparallel/20180822
+
+cat sample_names.txt | \
+parallel \
+--interactive \
+--verbose \
+--jobs 1 \
+--max-replace-args 1 \
+"sbatch STARmap_devel.sh {}"
