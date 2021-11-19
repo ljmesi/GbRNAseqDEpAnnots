1c3e0e3bf0c8d5d37d00f018518895524e7f14e2
ljmesi <37740329+ljmesi@users.noreply.github.com>
2021-11-19 15:59:23 +0100

    Simplify util functions

--- src/utils/utils.R
+++ src/utils/utils.R
@@ -27,15 +27,21 @@ merge_files <- function(dir_path, regex, header, cols){
     dataset
 }
 
-create_meta <- function(meta, sample_names){
-    Condition <- paste(meta$Condition, 
-                        meta$Phenotype, 
-                        sep = " ")
+replace_row_names_with_col <- function(df, col_name_to_row_name){
+  df %<>% remove_rownames %>% 
+        column_to_rownames(var = col_name_to_row_name)
+}
 
-    metadata <- data.frame(sample_names,Condition)
-
-    metadata %<>% remove_rownames %>% 
-        column_to_rownames(var = "sample_names")
+create_meta <- function(df){
+  sample_names <- paste(df$Condition, 
+                        df$Sample_ID, 
+                        sep = "-")
+  condition <- paste(df$Condition, 
+                     df$Phenotype, 
+                     sep = "_")
+  metadata <- data.frame(sample_names, condition, 
+                         row.names = 1, 
+                         stringsAsFactors = TRUE)
 }
 
 # Form a sample names vector
