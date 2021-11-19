04fabee5d0d3ca4f064e19fe2c55b949d17f260e
ljmesi <37740329+ljmesi@users.noreply.github.com>
2021-11-19 15:58:54 +0100

    Remove duplication and use precomputed objects

--- src/vis/DE/plot_pca_transform_DESeq.R
+++ src/vis/DE/plot_pca_transform_DESeq.R
@@ -8,64 +8,49 @@ library(DESeq2)
 library(tidyverse)
 library(ggrepel)
 
-snakemake@source(here("src","utils","fig_params.R"))
-
 set.seed(1)
 
-dds <- readRDS(snakemake@input[["normalised_counts_DESeq"]])
+save_fig <- function(file_name, plot_object){
+  ggsave(
+    filename = file_name,
+    plot = plot_object,
+    device = "svg",
+    width = width_in, 
+    height = height_in,
+    units = "in",
+    dpi = dpi_no,
+    limitsize = TRUE)
+}
 
+create_pca_plot <- function(transformed_obj, plot_title, samples){
+
+  sample_labels <- data.frame(samples)
+
+  # Plot PCA
+  pca_plot <- plotPCA(transformed_obj, intgroup = "condition") +
+                      labs(title = plot_title)
+
+  pca_plot$data <- cbind(pca_plot$data,sample_labels)
+
+  # # Add text lables to the points
+  pca_plot <- pca_plot +
+    geom_text_repel(aes(label = samples))
+}
+
+snakemake@source(here("src","utils","fig_params.R"))
 
 meta <- read_tsv(file = snakemake@input[["metadata"]],
                  col_types = "ccc")
-
-sample_labels <- data.frame(samples = meta$Sample_ID)
-
-print(sample_labels)
-
-#### Transform the data so that it's better visualised with heatmaps ####
-rld <- rlog(dds, 
-            blind=TRUE)
-
-# Plot PCA
-pca_plot <- plotPCA(rld, intgroup = "Condition") +
-                    labs(title = "PCA of normalised and sample-wise rlog transformed counts")
-
-pca_plot$data <- cbind(pca_plot$data,sample_labels)
-
-# # Add text lables to the points
-pca_plot <- pca_plot +
-  geom_text_repel(aes(label = samples))
-
-ggsave(
-  filename = snakemake@output[["rlog_transformed_pca"]],
-  plot = last_plot(),
-  device = "svg",
-  width = width_in, 
-  height = height_in,
-  units = "in",
-  dpi = dpi_no,
-  limitsize = TRUE)
+samples = meta$Sample_ID
+print(samples)
 
 
-vstd <- vst(dds, 
-           blind=TRUE)
+#### Use transformed data which works better with PCA ####
+rld <- readRDS(snakemake@input[["rlog_transformed"]])
+rld_plt <- create_pca_plot(rld, "PCA of normalised and sample-wise rlog transformed counts", samples)
+save_fig(snakemake@output[["rlog_transformed_pca"]], rld_plt)
 
 
-pca_plot <- plotPCA(vstd, intgroup = "Condition") +
-                    labs(title = "PCA of normalised and sample-wise variance stabilisation transformed counts")
-
-pca_plot$data <- cbind(pca_plot$data,sample_labels)
-
-# # Add text lables to the points
-pca_plot <- pca_plot +
-  geom_text_repel(aes(label = samples))
-
-ggsave(
-  filename = snakemake@output[["vs_transformed_pca"]],
-  plot = last_plot(),
-  device = "svg",
-  width = width_in, 
-  height = height_in,
-  units = "in",
-  dpi = dpi_no,
-  limitsize = TRUE)
+vstd <- readRDS(snakemake@input[["vs_transformed"]])
+vstd_plt <- create_pca_plot(vstd, "PCA of normalised and sample-wise variance stabilisation transformed counts", samples)
+save_fig(snakemake@output[["vs_transformed_pca"]], vstd_plt)
