2ddcac12825fc106b02799664dab2dc23dcb57ae
ljmesi <37740329+ljmesi@users.noreply.github.com>
2021-12-01 20:25:45 +0100

    Remove unused script

--- src/utils/csvkit_log.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://jtrocinski.com/posts/Bash-Error_handling_in_scripts.html
-set -euo pipefail
-
-echo -e "csvstat $1 $2:\n" &&
-csvstat "$1" "$2" &&
-echo "-----------------------------------------" &&
-echo -e "csvlook $1 $2:\n" &&
-csvlook "$1" "$2" | sed -n 1,10p
