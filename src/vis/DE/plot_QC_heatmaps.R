69d1a0a22c3c9c5c2618db11102da0abc8666e10
ljmesi <37740329+ljmesi@users.noreply.github.com>
2021-12-01 20:25:33 +0100

    Clean QC heatmaps script

--- src/vis/DE/plot_QC_heatmaps.R
+++ src/vis/DE/plot_QC_heatmaps.R
@@ -9,52 +9,46 @@ library(pheatmap)
 library(RColorBrewer)
 
 snakemake@source(here("src","utils","fig_params.R"))
+snakemake@source(here("src","utils","utils.R"))
 
 ### Set a colour palette
 heat_colours <- brewer.pal(9, "Blues")
 
-dds <- readRDS(snakemake@params[[1]])
-
 transformed_data <- readRDS(snakemake@input[[1]])
+transformed_assay <- assay(transformed_data)
 
-# Indexes of 20 highest means of normalised counts of all samples from all genes
-select <- order(rowMeans(counts(dds,normalized=TRUE)),
-                decreasing=TRUE)[1:20]
+print("Transformed data assay:")
+head(transformed_assay)
 
 # Tell which samples belong to which condition (important for annotation)
-condition <- colData(dds)[,c("condition")]
-annotation <- as.data.frame(condition)
-rownames(annotation) <- rownames(colData(dds))
+annotation <- create_annotation(transformed_data)
 
-# Open file
+# Indexes of "params" highest means of transformed counts of all samples from all genes
+num_highest_means <- as.integer(snakemake@params[["num_highest_means"]])
+select <- order(rowMeans(transformed_assay),decreasing=TRUE)[1:num_highest_means]
+
+#### Plot count matrix ####
 svg(snakemake@output[["count_matrix"]], 
     width = width_in, 
     height = height_in)
 
-
-normalised_counts <- assay(transformed_data)
-
-
-# Create the plot
-pheatmap(normalised_counts[select,],
+pheatmap(transformed_assay[select,],
          color = heat_colours,
          cluster_rows=FALSE, 
          show_rownames=FALSE,
          cluster_cols=FALSE, 
          annotation_col=annotation)
-
-# Close the file
 dev.off()
 
 # Calculate euclidian distance matrix between all samples
-sampleDists <- dist(t(normalised_counts), method = "euclidean")
+sampleDists <- dist(t(transformed_assay), method = "euclidean")
 sampleDistMatrix <- as.matrix(sampleDists)
 rownames(sampleDistMatrix) <- row.names(sampleDistMatrix)
 colnames(sampleDistMatrix) <- NULL
 
 colors <- colorRampPalette(brewer.pal(9, "Blues"))(255)
 
-# Open file
+#### Plot sample to sample distance matrix
 svg(snakemake@output[["sample_to_sample_distances"]], 
     width = width_in, 
     height = height_in)
@@ -63,8 +57,6 @@ pheatmap(sampleDistMatrix,
          clustering_distance_rows=sampleDists,
          clustering_distance_cols=sampleDists,
          col=colors)
-
-# Close the file
 dev.off()
 
 print("DESeq2 model dispersion plot drawn")
