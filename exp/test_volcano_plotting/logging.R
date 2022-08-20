2ba7b99b3ec1353d7fa8e5748b1cfb7c302ab2e2
Lauri Mesilaakso <john.mesilaakso@gmail.com>
2022-08-20 16:28:11 +0300

    Delete exp/test_volcano_plotting directory

--- exp/test_volcano_plotting/logging.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-
-log <- file(snakemake@log[[1]], open="wt")
-# Divert output to log file
-sink(log)
-# Divert messages, warning and stop to log file
-sink(log, type="message")
